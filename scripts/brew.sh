abb7466ace04f8c2c9fc6100cbbd2689ef7695b8
Luke Georgette <lukefg@users.noreply.github.com>
2021-05-09 16:29:49 -0700

    Add different Brewfiles for each use case

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -1,23 +1,34 @@
 #!/bin/sh
 
+BREWFILE_DIR="scripts/brewfiles"
+MY_BREWFILES="Brewfile Brewfile-mine-gaming Brewfile-mine-music Brewfile-work"
+
 echo "Updating Homebrew..."
 brew update
 echo "Upgrading any pre-existing Homebrew packages or apps..."
 brew upgrade
 
-echo "Executing Brewfile..."
-brew bundle --file scripts/Brewfile
-# Automatically installs the bundle "tap" on first run
+echo "Executing Brewfiles..."
+echo "Changing working directory to ${BREWFILE_DIR}..."
+cd ${BREWFILE_DIR}
+for FILE in ${MY_BREWFILES}
+do
+	echo "Now starting: ${FILE}..."
+	brew bundle --file="${FILE}"
+	# Automatically installs the bundle "tap" on first run
 
-echo "Checking if everything was installed..."
-brew bundle check --file scripts/Brewfile
-if [ $? -eq 0 ]
-then
-	echo "Success!"
-else
-	echo "Something's missing. Retry later."
-	return 1
-fi
+	echo "Checking if everything was installed..."
+	brew bundle check --file="${FILE}"
+	if [ $? -eq 0 ]
+	then
+		echo "Success!"
+	else
+		echo "Something's missing from ${FILE}. Exiting. Retry later."
+		cd "${REPO_DIR}"
+		return 1
+	fi
+done
 
+cd "${REPO_DIR}"
 return 0
 
