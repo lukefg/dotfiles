bbd750a13df06cade128c0b1ddc96d7dbdd364a6
Luke Georgette <lukefg@users.noreply.github.com>
2021-05-09 01:37:29 -0700

    Add partial Brewfile, add Xcode CLT, ignore Brewfile locks

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 
-echo "Installing Homebrew formulae..."
+echo "Updating Homebrew..."
+brew update
+echo "Upgrading any pre-existing Homebrew packages or apps..."
+brew upgrade
 
-echo "Installing Homebrew casks..."
+echo "Executing Brewfile..."
+brew bundle --file scripts/Brewfile
+# Automatically installs the bundle "tap" on first run
 
-echo "Installing Mac App Store applications..."
+echo "Checking if everything was installed..."
+brew bundle check --file scripts/Brewfile
+if [ $? -eq 0 ]
+then
+	echo "Success!"
+else
+	echo "Something's missing. Retry later."
+	return 1
+fi
 
 return 0
 
