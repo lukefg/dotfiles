a4605ea3aeadfd7554e7faf7544e2c0dbf42ca7e
Luke Georgette <lukefg@users.noreply.github.com>
2021-05-08 21:06:13 -0700

    Add comments, fill simple configs, actually symlink them

--- scripts/linker.sh
+++ scripts/linker.sh
@@ -4,14 +4,14 @@ echo "Symlinking configurations to ${HOME}..."
 
 for DOTFILE in $(find "${REPO_DIR}/config/linked")
 do
-	if [ -f ${DOTFILE} ]
+	if [ -f ${DOTFILE} ] # file exists
 	then
 
-		# check if there already is one: a real file, or a symlink
+		# check if there already is one in HOME
 		DOTFILE_BASENAME=$(basename "${DOTFILE}")
-		if [ -f "${HOME}/${DOTFILE_BASENAME}" ]
+		if [ -f "${HOME}/${DOTFILE_BASENAME}" ] # file exists
 		then
-			if [ -L "${HOME}/${DOTFILE_BASENAME}" ]
+			if [ -L "${HOME}/${DOTFILE_BASENAME}" ] # is symlink
 			then
 				MESSAGE_PREFIX="${DOTFILE_BASENAME} is already symlinked to HOME."
 			else
@@ -21,6 +21,7 @@ do
 			while true
 			do
 				read -p "${MESSAGE_PREFIX} Are you sure? [yN] " RESPONSE
+				# -p inline prompt
 				case $RESPONSE in
 					[Yy]* ) break;;
 					* ) echo "Canceling. Retry later."; return 1;;
@@ -29,10 +30,11 @@ do
 		fi
 
 		echo "Linking $(basename "${DOTFILE}") to home..."
-		# ln -fs "${DOTFILE}" "${HOME}"
+		ln -fs "${DOTFILE}" "${HOME}"
+		# -s: symbolic
+		# -f: if it already exists, replace it
 	fi
 done
-
 
 unset DOTFILE
 unset DOTFILE_BASENAME
