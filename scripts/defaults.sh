ef61dd1958c85cadebb4f62e3a7b4223f3d58d7d
Luke Georgette <lukefg@users.noreply.github.com>
2021-05-09 21:04:10 -0700

    Framework for defaults script, change test aliases to be useful during this part

--- scripts/defaults.sh
+++ scripts/defaults.sh
@@ -2,4 +2,29 @@
 
 echo "Modifying macOS preferences..."
 
+echo "Closing System Preferences..."
+osascript -e 'tell application "System Preferences" to quit'
+
+# echo "Requesting superuser privileges..."
+# sudo -v
+# echo "Keeping sudo alive until done..."
+# while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+
+
+
+
+
+
+APPS="" # @TODO
+echo "Killing all affected applications..."
+for APP in $APPS;
+do
+    echo "Killing ${APP}..."
+    killall "${APP}"
+done
+
+echo "Done. Note: Some changes require a logout/restart to take effect."
+
+
 return 0
