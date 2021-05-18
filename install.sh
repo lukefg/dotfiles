ac88064ed639e82ea41f44cd8f0cdaa0c2ee54d5
Luke Georgette <lukefg@users.noreply.github.com>
2021-05-18 10:41:52 -0700

    Add control flow to determine what package groups to install

--- install.sh
+++ install.sh
@@ -5,6 +5,46 @@ CLONE_DIR="$HOME/Repos/git"
 
 echo "Bootstrapping new machine..."
 
+while true
+do
+	read -p "Is this a [p]ersonal or [w]ork machine? " RESPONSE
+	case $RESPONSE in
+		[Pp]* ) echo "This is a personal machine."; break;;
+		[Ww]* ) echo "This is a work machine."; INSTALL_WORK=0; break;;
+		* ) echo "Need a response starting with P or W. Try again.";;
+	esac
+done
+
+if [ ${INSTALL_WORK} ]
+then
+	read -p "Install Choices package? [yN] " RESPONSE
+	case $RESPONSE in
+		[Yy]* ) echo "Will install Choices requirements."; INSTALL_CHOICES=0;;
+		* ) echo "Will not install Choices requirements.";
+	esac
+	read -p "Install 3D package? [yN] " RESPONSE
+	case $RESPONSE in
+		[Yy]* ) echo "Will install 3D requirements."; INSTALL_3D=0;;
+		* ) echo "Will not install 3D requirements.";
+	esac
+	read -p "Install 1001 package? [yN] " RESPONSE
+	case $RESPONSE in
+		[Yy]* ) echo "Will install 1001 requirements."; INSTALL_1001=0;;
+		* ) echo "Will not install 1001 requirements.";
+	esac
+else
+	read -p "Install gaming package? [yN] " RESPONSE
+	case $RESPONSE in
+		[Yy]* ) echo "Will install gaming requirements."; INSTALL_GAMING=0;;
+		* ) echo "Will not install gaming requirements.";
+	esac
+	read -p "Install music production package? [yN] " RESPONSE
+	case $RESPONSE in
+		[Yy]* ) echo "Will install music production requirements."; INSTALL_MUSIC=0;;
+		* ) echo "Will not install music production requirements.";
+	esac
+fi
+
 # First, check Software Update
 echo "Running Software Update..."
 softwareupdate --all --install
--- scripts/brew.sh
+++ scripts/brew.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+function check_brewfile() {
+
+	if [ -z $1 ]
+	then
+		echo "ERROR: check_brewfile() needs an argument."
+		return 1
+	fi
+
+	if [ ! -r $1 ]
+	then
+		echo "ERROR: $1 is not readable."
+		return 2
+	fi
+
+	echo "Checking if everything in $1 is installed already..."
+	brew bundle check --file="$1"
+	if [ $? -eq 0 ]
+	then
+		echo "Success!"
+	else
+		echo "Something's missing from $1. Brewing..."
+		brew bundle --file="$1"
+	fi
+}
+
 BREWFILE_DIR="scripts/brewfiles"
 MY_BREWFILES="Brewfile Brewfile-mine-gaming Brewfile-mine-music Brewfile-work"
 
@@ -8,21 +33,17 @@ brew update
 echo "Upgrading any pre-existing Homebrew packages or apps..."
 brew upgrade
 
-echo "Executing Brewfiles..."
 echo "Changing working directory to ${BREWFILE_DIR}..."
 cd ${BREWFILE_DIR}
-for FILE in ${MY_BREWFILES}
-do
-	echo "Checking if everything in ${FILE} is installed already..."
-	brew bundle check --file="${FILE}"
-	if [ $? -eq 0 ]
-	then
-		echo "Success!"
-	else
-		echo "Something's missing from ${FILE}. Brewing..."
-		brew bundle --file="${FILE}"
-	fi
-done
+echo "Executing Brewfiles based on requirements..."
+
+check_brewfile Brewfile
+test $INSTALL_GAMING && check_brewfile Brewfile-mine-gaming
+test $INSTALL_MUSIC && check_brewfile Brewfile-mine-music
+test $INSTALL_WORK && check_brewfile Brewfile-work
+test $INSTALL_CHOICES && check_brewfile Brewfile-work-choices
+test $INSTALL_3D && check_brewfile Brewfile-work-3d
+test $INSTALL_1001 && check_brewfile Brewfile-work-1001
 
 echo "Cleaning up..."
 brew cleanup
